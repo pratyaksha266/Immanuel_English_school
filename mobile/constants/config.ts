468a766476f983205a31f05c8d6b6110cfa98710
pratyaksha266 <pratyakshamajalikar@gmail.com>
2026-01-09 02:01:55 +0530

    Update mobile app to use live production URL

--- mobile/constants/config.ts
+++ mobile/constants/config.ts
@@ -3,7 +3,7 @@
 // Use 'http://localhost:8000/api/' for iOS Simulator
 
 // DEVELOPMENT (Uncomment to use)
-export const API_URL = 'http://10.93.67.212:8000/api/';
+// export const API_URL = 'http://10.93.67.212:8000/api/';
 
 //PRODUCTION (Uncomment to use for deployment)
-// export const API_URL = 'https://outpassapp.onrender.com/api/';
+export const API_URL = 'https://immanuel-english-school-backend.onrender.com/api/';
